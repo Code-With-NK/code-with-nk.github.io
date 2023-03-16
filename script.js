0357ebe500d568287df5c9571d78381969f0d71a
CodeWithNK <118751244+Code-With-NK@users.noreply.github.com>
2023-03-16 09:41:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,60 @@
+let bgNum = 1
+const container = document.querySelector('.project-container')
+
+addProject('ludo')
+addProject('memory-puzzle')
+addProject('2048')
+addProject('snake')
+addProject('tetris')
+addProject('connect-four')
+addProject('crossword-puzzle')
+
+document.querySelectorAll('.list').forEach((item) => {
+    item.onclick = () => {
+        const value = item.dataset.filter
+        
+        document.querySelectorAll('.project-box').forEach((project) => {
+            if (!project.classList.contains(value)) {
+                project.style.opacity = 0
+            } else {
+                project.style.opacity = 1
+            }
+        })
+    }
+})
+
+function addProject(repo) {
+    const link = "https://code-with-nk.github.io/" + repo
+    
+    container.innerHTML += `
+        <project href="" class="project-box web all" data-repo="${link}">
+                 <img src="/images/bg-${getBg()}.jpg" alt="">
+                 <text>${repo}</text>
+        </project>
+    `
+    
+    document.querySelectorAll('.project-box').forEach((img) => {
+        img.onclick = () => {
+            location.assign(img.dataset.repo)
+        }
+    })
+}
+
+function getBg(){
+    if(bgNum == 4){
+        bgNum = 1
+    } else {
+        bgNum++
+    }
+    
+    return bgNum
+}
+
+document.querySelectorAll('.project-filter li').forEach((icon) => {
+    icon.onclick = () => {
+        document.querySelectorAll('.project-filter li').forEach((icon) => {
+            icon.classList.remove('project-filter-active')
+        })
+        icon.classList.add('project-filter-active')
+    }
+})
